f6a3ff22b292eda40681e552b9fb72879939577d
Edgar Go <goedgaraaron@gmail.com>
2025-12-28 10:55:00 +0800

    added all the needed subparsers

--- Expense Tracker/expense_tracker.py
+++ Expense Tracker/expense_tracker.py
@@ -10,11 +10,33 @@ subparsers = parser.add_subparsers(dest="command", required=True)
 add_parser = subparsers.add_parser("add")
 add_parser.add_argument("--description", required=True)
 add_parser.add_argument("--amount", type=int, required=True)
+add_parser.add_argument("--category", required=False, default="General")
 add_parser.add_argument("--date", required=False, default=datetime.datetime.now())                   # Used for testing. Defaults to current date.
 
 # Update
 
+update_parser = subparsers.add_parser("update")
+update_parser.add_argument("--id", type=int, required=True)
+update_parser.add_argument("--amount", type=int, required=True)
 
+# Delete
+
+delete_parser = subparsers.add_parser("delete")
+delete_parser.add_argument("--id", type=int, required=True)
+
+# List
+
+list_parser = subparsers.add_parser("list")
+
+# Summary
+
+summary_parser = subparsers.add_parser("summary")
+summary_parser.add_argument("--month", type=int, required=False)
+
+# Budget
+
+budget_parser = subparsers.add_parser("budget")
+budget_parser.add_argument("--amount", type=int, required=True)
 
 # Main
 
@@ -22,3 +44,15 @@ args = parser.parse_args()
 
 if args.command == "add":
     print(f"Add {args.description} with amount {args.amount} at {args.date}")
+elif args.command == "update":
+    pass
+elif args.command == "delete":
+    pass
+elif args.command == "list":
+    pass
+elif args.command == "summary":
+    pass
+elif args.command == "budget":
+    print(f"You have updated your monthly budget from {budget} to {args.amount}")
+else:
+    print("An error has occured.\n")
